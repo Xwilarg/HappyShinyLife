9296d32266f5322c8eaed748998a27b0810c0809
Xwilarg <xwilarg@protonmail.com>
2023-01-29 14:46:08 +0100

    Add player controls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace HappyShinyLife
 {
     public class PlayerController : MonoBehaviour
     {
+        private Vector2 _mov;
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = new Vector2(_mov.x, _rb.velocity.y);
+        }
+
+        public void OnMove(InputAction.CallbackContext value)
+        {
+            _mov = value.ReadValue<Vector2>().normalized;
+        }
     }
 }
